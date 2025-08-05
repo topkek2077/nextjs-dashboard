43ea484123ae918bacbd64e9c02d8c4c7b031ed8
lg0leg <lg.oleg1067@gmail.com>
2025-08-05 16:46:03 +0300

    фикс турбопака

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next';
+const path = require('path')
 
 const nextConfig: NextConfig = {
     experimental: {
         ppr: 'incremental'
-    }
+    },
+    turbopack: {
+        root: path.join(__dirname, '..'),
+    },
 };
 
 export default nextConfig;
